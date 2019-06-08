6b3df79bf24ccee8085a2b3fe8cf4745a994fbea
Nasrin Eshraghi Ivari <nasrin.eshraghi@unb.ca>
2019-06-08 20:25:13 -0300

    test in IEEE Verilog 2005

---
+++ ODIN_II/regression_test/benchmark/operators/final-pca.v
@@ -0,0 +1,19 @@
+
+module dff (q, d, clear, preset, clock);
+
+output q;
+input d, clear, preset, clock;
+reg q;
+
+always @(clear or preset)
+    if (!clear)
+        assign q = 0;
+    elseif (!preset)
+        assign q = 1;
+    else
+        deassign q;
+
+always @(posedge clock)
+    q = d;
+
+endmodule
