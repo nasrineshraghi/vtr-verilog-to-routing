6d316d37d3c3df7e4a4473f97826edaf4c8f205b
Nasrin Eshraghi Ivari <nasrin.eshraghi@unb.ca>
2019-06-09 20:26:58 -0300

    All supported tests added

---
+++ ODIN_II/regression_test/benchmark/operators/pca_2_always_block_reset.v
@@ -0,0 +1,29 @@
+module FlipflopAssign ( reset, din, clk, qout);
+
+	output qout;
+
+	input reset;
+	input din;
+	input clk;
+
+	always @ (reset) 
+	begin
+
+	if (reset)
+
+		assign qout = 0;
+
+	else 
+
+		deassign qout;
+
+	end
+
+	always @ (posedge clk) 
+	begin
+
+		qout = din;
+
+	end
+
+endmodule
